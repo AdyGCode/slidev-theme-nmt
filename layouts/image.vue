95650448f1fdb2cf00d4bf5fdc1ab3c7acc66404
Adrian Gould <adrian.gould@nmtafe.wa.edu.au>
2026-02-24 14:15:28 +0800

    feat: add image slide

---
+++ layouts/image.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="slidev-layout default h-full">
+    <slot />
+    <ImageWithOptionalCaption
+      class="h-5/6 p-4"
+      :caption="imageCaption"
+      :footnoteNumber="imageFootnoteNumber"
+      :url="imageUrl"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+defineProps<{
+  imageCaption?: string;
+  imageFootnoteNumber?: number;
+  imageUrl: string;
+}>();
+</script>
